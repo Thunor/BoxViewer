5fd45664f99ff7c20237ac019e5f3114909a94f2
Eric Freitas <efreitos@gmail.com>
2023-01-31 10:38:07 -0600

    Initial Commit

---
+++ BoxViewer/BoxViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoxViewerApp.swift
+//  BoxViewer
+//
+//  Created by Eric Freitas on 1/31/23.
+//
+
+import SwiftUI
+
+@main
+struct BoxViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
